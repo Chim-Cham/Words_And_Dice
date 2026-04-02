73a2ac5a1c4c29411c0cb891c345931878ee7fab
Jenjira <jenjira.h04@gmail.com>
2026-04-02 18:07:55 +0200

    Added an copy icon for input Game ID

--- frontend/src/Pages/StartPage.tsx
+++ frontend/src/Pages/StartPage.tsx
@@ -1,10 +1,22 @@
 import "../css/StartPage.css";
+import copyIcon from "../assets/copy.svg";
 
 export function StartPage() {
   return (
     <div className="start-container">
-      <h1>Words & Dice</h1>
+      <h1 className="title">Words & Di<span className="parenthese">c</span>e</h1>
       <p className="tag-line">A challenging word game for 2 players</p>
+
+      <div className="input-wrapper">
+        <input
+          type="text"
+          placeholder="Game ID"
+          className="game-id"
+        />
+        <button className="copy-btn">
+          <img src={copyIcon} alt="Copy" />
+        </button>
+      </div>
 
       <div className="button-group">
         <button className="btn Start">Start Game</button>
