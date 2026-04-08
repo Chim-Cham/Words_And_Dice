e99f0644327ce5123ba54c95fd3cfc27a8fb4a6c
OscarK99Swe <oscar.kock99@gmail.com>
2026-04-08 20:26:10 +0200

    Added new file
    Added a new file along with temporary words

---
+++ frontend/src/data/words.ts
@@ -0,0 +1,37 @@
+export type WordEntry = {
+  word: string;
+  length: number;
+  category: string;
+  language: string;
+};
+
+export const WORDS: WordEntry[] = [
+
+  {
+    "word": "skibidi",
+    "length": 7,
+    "category": "brainrot",
+    "language": "en"
+  },
+  {
+    "word": "gyatt",
+    "length": 5,
+    "category": "brainrot",
+    "language": "en"
+  },
+  {
+    "word": "ohio",
+    "length": 4,
+    "category": "brainrot",
+    "language": "en"
+  },
+
+
+
+
+
+
+
+
+
+];
