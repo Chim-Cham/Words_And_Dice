f845f28cfc0fbb9a3b20b59420f25c9d97fb2e65
Chim-Cham <gustavfransson@hotmail.com>
2026-04-10 11:12:48 +0200

    API and API test in working state

--- backend.Tests/ApiTest.cs
+++ backend.Tests/ApiTest.cs
@@ -1,30 +1,33 @@
 using Xunit;
 namespace backend.Tests;
 using System.Text.Json;
-
+using Microsoft.AspNetCore.Builder;
 
 public class ApiTest
 {
 
-    private readonly HttpClient _client;
 
     [Fact]
     public async Task wordFetch()
     {
+        using var fetch = new HttpClient();
+
         // Fetch from API
-        var response = await _client.GetAsync("/api/word/animals/5");
+        // var response = await fetch.GetAsync("https://random-words-api.kushcreates.com/api?language=en&category=animals&length=5");
+        var response = await fetch.GetAsync("http://localhost:5164/api/word/animals/5");
+
 
         // Check fetch was succesful
         response.EnsureSuccessStatusCode();
 
         // Converts the fetched Json data into a string format
         var content = await response.Content.ReadAsStringAsync();
-        var words = JsonSerializer.Deserialize<string>(content);
+        // var words = JsonSerializer.Deserialize<string>(content);
 
-        System.Console.WriteLine(words);
+        System.Console.WriteLine(content);
 
         // Checks if the fetch was not null or empty
-        Assert.NotNull(words);
-        Assert.NotEmpty(words);
+        Assert.NotNull(content);
+        Assert.NotEmpty(content);
     }
 }
