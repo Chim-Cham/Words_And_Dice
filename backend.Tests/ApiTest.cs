4bb4365a0aca77f5382b502d8286e56cdce0117a
Chim-Cham <gustavfransson@hotmail.com>
2026-04-10 10:46:31 +0200

    refined test setup for API

--- backend.Tests/ApiTest.cs
+++ backend.Tests/ApiTest.cs
@@ -1,14 +1,15 @@
-using System.Text.Json;
 using Xunit;
 namespace backend.Tests;
-using Microsoft.AspNetCore.Mvc.Testing;
+using System.Text.Json;
+
 
 public class ApiTest
 {
+
     private readonly HttpClient _client;
 
     [Fact]
-    public async Task fetchTest()
+    public async Task wordFetch()
     {
         // Fetch from API
         var response = await _client.GetAsync("/api/word/animals/5");
@@ -16,11 +17,14 @@ public class ApiTest
         // Check fetch was succesful
         response.EnsureSuccessStatusCode();
 
+        // Converts the fetched Json data into a string format
         var content = await response.Content.ReadAsStringAsync();
         var words = JsonSerializer.Deserialize<string>(content);
 
+        System.Console.WriteLine(words);
+
+        // Checks if the fetch was not null or empty
         Assert.NotNull(words);
         Assert.NotEmpty(words);
-
     }
 }
