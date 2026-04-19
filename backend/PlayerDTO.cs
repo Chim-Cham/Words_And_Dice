805ceea1d8b3ed788635cc834b615103c8b8e8fa
Jenjira <jenjira.h04@gmail.com>
2026-04-20 00:29:46 +0200

    Added guessrequest and guessresult DTOs to playerDTO.cs
    These will be used by the new guess endpoint and score update logic.
    DownScore logic will be implemented in GameService and RoundApi.cs

--- backend/PlayerDTO.cs
+++ backend/PlayerDTO.cs
@@ -9,3 +9,16 @@ public class PlayerDto
     public string? LastGuess { get; set; }
     public bool IsRoundReady { get; set; }
 }
+
+public class GuessRequest
+{
+    public string PlayerId { get; set; } = "";
+    public string Guess { get; set; } = "";
+}
+
+public class GuessResult
+{
+    public bool Correct { get; set; }
+    public int ScoreChange { get; set; }
+    public int NewScore { get; set; }
+}
